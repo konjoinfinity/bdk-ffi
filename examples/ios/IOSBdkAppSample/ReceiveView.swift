a9d259a93ca2811642713656b0b5768deb43a3c9
Paul Miller <paul@paul.lol>
2021-11-05 11:09:32 -0400

    skinned app

--- examples/ios/IOSBdkAppSample/ReceiveView.swift
+++ examples/ios/IOSBdkAppSample/ReceiveView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct ReceiveView: View {
     var address: String;
+    
     var body: some View {
-        VStack {
-            Text("your wallet receive address")
-            Text(address)
-                .font(.largeTitle)
-        }.navigationBarTitle("Receive")
+        BackgroundWrapper {
+            Spacer()
+            VStack {
+                Rectangle().fill(.white).cornerRadius(5).frame(width: 200, height: 200)
+                Text(address).textStyle(BasicTextStyle(white: true))
+            }.contextMenu {
+                Button(action: {
+                    UIPasteboard.general.string = address}) {
+                        Text("Copy to clipboard")
+                    }
+            }
+            Spacer()
+            BasicButton(action: {}, text: "Generate new address", color: "Green")
+        }
+        .navigationTitle("Receive Address")
+        .modifier(BackButtonMod())
     }
 }
 
--- examples/ios/IOSBdkAppSample/SendView.swift
+++ examples/ios/IOSBdkAppSample/SendView.swift
@@ -9,32 +9,56 @@ import SwiftUI
 import Combine
 
 struct SendView: View {
+    
+    
+    
     var onSend : (String, UInt64) -> ()
     @Environment(\.presentationMode) var presentationMode
     @State var to: String = ""
     @State var amount: String = "0.000"
+    
+    
+    
     var body: some View {
-        Form {
-            Section(header: Text("Recipient")) {
-                TextField("Address", text: $to)
-            }
-            Section(header: Text("Amount (BTC)")) {
-                TextField("Amount", text: $amount)
-                    .keyboardType(.numberPad)
-                    .onReceive(Just(amount)) { newValue in
-                        let filtered = newValue.filter { "0123456789.".contains($0) }
-                        if filtered != newValue {
-                            self.amount = filtered
+        BackgroundWrapper {
+            VStack {
+                
+            
+            Form {
+                Section(header: Text("Recipient").textStyle(BasicTextStyle(white: true))) {
+                    TextField("Address", text: $to)
+                        .modifier(BasicTextFieldStyle())
+                }
+                Section(header: Text("Amount (BTC)").textStyle(BasicTextStyle(white: true))) {
+                    TextField("Amount", text: $amount)
+                        .modifier(BasicTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .onReceive(Just(amount)) { newValue in
+                            let filtered = newValue.filter { "0123456789.".contains($0) }
+                            if filtered != newValue {
+                                self.amount = filtered
+                            }
                         }
-                    }
+                }
+//                BasicButton(action: {
+//                    onSend(to, UInt64((Double(amount) ?? 0) * Double(100000000)))
+//                    presentationMode.wrappedValue.dismiss()
+//                }, text: "Send")
+//                    .disabled(to == "" || (Double(amount) ?? 0) == 0)
             }
-            Section {
-                Button("Send") {
+            .onAppear {
+                UITableView.appearance().backgroundColor = .clear }
+                
+                Spacer()
+                BasicButton(action: {}, text: "Scan Address")
+                BasicButton(action: {
                     onSend(to, UInt64((Double(amount) ?? 0) * Double(100000000)))
                     presentationMode.wrappedValue.dismiss()
-                }.disabled(to == "" || (Double(amount) ?? 0) == 0)
+                }, text: "Broadcast Transaction", color: "Red").disabled(to == "" || (Double(amount) ?? 0) == 0)
             }
-        }.navigationBarTitle("Send")
+        }
+        .navigationTitle("Send")
+        .modifier(BackButtonMod())
     }
 }
 
